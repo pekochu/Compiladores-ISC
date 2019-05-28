f8f09a27e1c3a11ff89eec931ba6794d65418fb7
Pekochu on Windows <lbravol1300@alumno.ipn.mx>
2019-05-28 15:46:02 -0500

    added fnctptr

--- BISON/MFCALC/calc.h
+++ BISON/MFCALC/calc.h
@@ -1,21 +1,21 @@
-/* Function type. */
-typedef double (func_t) (double);
+/* Function type.  */
+typedef double (*func_t) (double);
 
-/* Data type for links in the chain of symbols. */
+/* Data type for links in the chain of symbols.  */
 struct symrec{
     char *name;  /* name of symbol */
-    int type;    /* type of symbol: either VAR or FUN */
-    union{
-        double var;    /* value of a VAR */
-        func_t *fun;   /* value of a FUN */
+    int type;    /* type of symbol: either VAR or FNCT */
+    union {
+        double var;      /* value of a VAR */
+        func_t fnctptr;  /* value of a FNCT */
     } value;
     struct symrec *next;  /* link field */
 };
 
 typedef struct symrec symrec;
 
-/* The symbol table: a chain of 'struct symrec'. */
+/* The symbol table: a chain of 'struct symrec'.  */
 extern symrec *sym_table;
- 
-symrec *putsym (char const *name, int sym_type);
-symrec *getsym (char const *name);
+
+symrec *putsym (char const *, int);
+symrec *getsym (char const *);
